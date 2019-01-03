1df7d48c4a620bf8667a821a7f2e5edab420572c
Andras <horinka.andras@gmail.com>
2019-01-03 14:41:34 +0100

    cleancode

--- src/com/codecool/snake/entities/enemies/SimpleEnemy.java
+++ src/com/codecool/snake/entities/enemies/SimpleEnemy.java
@@ -26,21 +26,15 @@ public class SimpleEnemy extends Enemy implements Animatable, Interactable {
         super(10);
 
         setImage(Globals.getInstance().getImage("SimpleEnemy"));
-
-//        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
-//        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
-
         boolean coordinatesX = rnd.nextBoolean();
         boolean coordinatesY = rnd.nextBoolean();
 
         if (coordinatesX) {
-//            if we spawn to the right from the snake
             this.xBound = (int)Math.round(Globals.WINDOW_WIDTH) - (int)Math.round(snakePos.x);
         } else {
 //            if we spawn to the left from the snake
             this.xBound = (int)Math.round(snakePos.x);
         }
-//        we set the X Position of the enemy
         if (this.xBound < 0 || this.xBound> (int)Math.round(Globals.WINDOW_WIDTH)) {
             this.xPos = (double)rnd.nextInt(500) + snakePos.x;
             this.xBound = 500;
@@ -49,20 +43,16 @@ public class SimpleEnemy extends Enemy implements Animatable, Interactable {
         }
 
         if (coordinatesY) {
-//            if we spawn below the snake
             this.yBound = (int)Math.round(Globals.WINDOW_HEIGHT) - (int)Math.round(snakePos.y);
         } else {
-//            if we spawn above the snake
             this.yBound = (int)Math.round(snakePos.y);
         }
-//        we set the Y Position of the enemy
         if (this.yBound < 0 || this.yBound > (int)Math.round(Globals.WINDOW_HEIGHT)) {
             this.yPos = snakePos.y + (double)rnd.nextInt(500);
             this.yBound = 500;
         } else {
             this.yPos = snakePos.y + (double)rnd.nextInt(yBound);
         }
-
 
         setX(this.xPos);
         setY(this.yPos);
